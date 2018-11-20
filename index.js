f27d316de702ca317a729be75eba94c476e6d213
m0py <mopihu@gmail.com>
2018-11-20 08:23:40 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,28 +1,24 @@
-const Command = require('command')
-const Vec3 = require('tera-vec3')
+const Vec3 = require('tera-vec3');
 
-module.exports = function HwRedirect(dispatch) {
-  const command = Command(dispatch)
+module.exports = function HwRedirect(mod) {
+  const highwatchRedeem = new Vec3(22205, 4870, 6191);
+  const highwatchBanker = new Vec3(22438, 1605, 5857);
 
-  const highwatchRedeem = new Vec3(22205, 4870, 6191)
-  const highwatchBanker = new Vec3(22438, 1605, 5857)
+  let enabled = true;
 
-  let enabled = true
-  let currentZone
-
-  dispatch.hook('S_LOAD_TOPO', 3, event => {
-    currentZone = event.zone
-  })
-
-  dispatch.hook('S_SPAWN_ME', 2, event => {
-    if (enabled && currentZone == 7031 && event.loc.equals(highwatchRedeem)) {
-      event.loc = highwatchBanker
+  mod.hook('S_SPAWN_ME', 3, event => {
+    if (enabled && mod.game.me.zone == 7031 && event.loc.equals(highwatchRedeem)) {
+      event.loc = highwatchBanker;
     }
-    return true
+    return true;
   })
 
-  command.add('hw', () => {
-    enabled = !enabled
-    command.message(enabled ? 'Highwatch Redirect enabled.' : 'Highwatch Redirect disabled.')
+  mod.command.add('hw', () => {
+    enabled = !enabled;
+    mod.command.message(enabled ? 'Highwatch Redirect enabled.' : 'Highwatch Redirect disabled.');
   })
+  
+  this.destructor = function() {
+    mod.command.remove('hw');
+  }
 }
